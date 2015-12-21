3b8a5c6c2dc462f3153329d690e5a91bfc620e51
Jens W. Klein <jk@kleinundpartner.at>
2015-12-21 23:11:31 +0100

    Back to development: 1.0a2

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ long_description = (
 
 setup(
     name='noeku.linguacollage',
-    version='1.0a1',
+    version='1.0a2.dev0',
     description="Helps with multilingual collages",
     long_description=long_description,
     # Get more from https://pypi.python.org/pypi?%3Aaction=list_classifiers
